c180d375390e83fa599a054933c095573c7265f3
amtat <amtat@DESKTOP-3DL3458>
2020-10-17 16:23:27 +0700

    PBOIF2-10119068-Latihan1-Biodata

---
+++ src/pboif2/pkg10119068/latihan1/biodata/PBOIF210119068Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119068.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * NAMA     : TATANG SULAEMAN
+ * KELAS    : PBOIF2
+ * NIM      : 10119068
+ * Deskripsi program    : menampilkan value ke layar
+ */
+public class PBOIF210119068Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "-------------------");
+        System.out.println("Nama\t\t: Tatang Sulaeman");
+        System.out.println("ALAMAT\t\t: Jalan Ciheulang no 103");
+        System.out.println("TEMPAT LAHIR\t: Karawang");
+        System.out.println("TANGGAL LAHIR\t: 25 Agustus 1999");
+        System.out.println("--------------------");
+        System.out.println("HOBI\t\t: Bermain PUBGM");
+        System.out.println("MAKES\t\t: Pepes Tahu");
+        System.out.println("MIKES\t\t: Nutrisari Florida Orange");
+        
+    }
+    
+}
